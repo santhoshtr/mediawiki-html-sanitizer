983b12c818bffffdbf5a2ff1b66efcb2ada6e8c9
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-02-13 15:49:02 +0530

    Extracted sanitizer from parsoid repo
    Lot of clean up, ES6 class update etc to follow.

---
+++ test/sanitizer.js
@@ -0,0 +1,34 @@
+/* global describe, it */
+
+'use strict';
+
+const should = require( 'chai' ).should(),
+	Sanitizer = require( '../lib/Sanitizer' ).Sanitizer,
+	defines = require( '../lib/parser.defines' );
+
+var TagTk = defines.TagTk;
+
+describe( 'Sanitizer', function () {
+	it( 'should sanitize attributes according to php\'s getAttribsRegex', function () {
+		let fakeEnv = {};
+		const sanitizer = new Sanitizer( fakeEnv );
+		let name = 'testelement';
+		sanitizer.attrWhiteListCache[ name ] = new Set( [
+			'foo', 'עברית', '६', '搭𨋢', 'ńgh'
+		] );
+		let token = new TagTk( name );
+		token.setAttribute( 'foo', 'bar' );
+		token.setAttribute( 'bar', 'foo' );
+		token.setAttribute( 'עברית', 'bar' );
+		token.setAttribute( '६', 'bar' );
+		token.setAttribute( '搭𨋢', 'bar' );
+		token.setAttribute( 'ńgh', 'bar' );
+		token = sanitizer.sanitizeToken( token );
+		token.getAttribute( 'foo' ).should.equal( 'bar' );
+		should.equal( token.getAttribute( 'bar' ), null );
+		token.getAttribute( 'עברית' ).should.equal( 'bar' );
+		token.getAttribute( '६' ).should.equal( 'bar' );
+		token.getAttribute( '搭𨋢' ).should.equal( 'bar' );
+		should.equal( token.getAttribute( 'ńgh' ), null );
+	} );
+} );
